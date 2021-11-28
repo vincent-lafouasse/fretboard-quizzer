259960d0026b2b43818c3c1cafea50266783a1b6
vincent-lafouasse <vincentlafouasse@live.fr>
2021-11-28 17:46:17 +0100

    test that AssertionError is raised when providing illegal tuning choice

--- test_fretboard.py
+++ test_fretboard.py
@@ -1,9 +1,25 @@
 import pytest
-import fretboard as fb
+from rich import inspect
+import fretboard as fret
 
 # Tuning
 def test_E_standard():
-    assert fb.set_tuning('E standard') == (4, 11, 7, 2, 9, 4,)
+    assert fret.set_tuning('E standard') == (4, 11, 7, 2, 9, 4,)
 
 def test_drop_D():
-    assert fb.set_tuning('Drop D') == (4, 11, 7, 2, 9, 2,)
+    assert fret.set_tuning('Drop D') == (4, 11, 7, 2, 9, 2,)
+
+def test_invalid_tuning():
+    tuning = (1,)
+    # inspect(tuning)
+    try:
+        tuning = fret.set_tuning('420')
+    except AssertionError:
+        tuning = None
+    # inspect(tuning)
+    # inspect(tuning is None)
+    assert tuning is None
+
+
+if __name__ == '__main__':
+    test_invalid_tuning()
