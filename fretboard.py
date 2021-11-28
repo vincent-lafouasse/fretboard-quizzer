0f477e4e971d3c8dd804c7b7ecad7b9bd1f53df5
vincent-lafouasse <vincentlafouasse@live.fr>
2021-11-28 14:36:50 +0100

    formatting source

--- fretboard.py
+++ fretboard.py
@@ -10,61 +10,76 @@ def main():
     while 1:
         note, string = random_note_and_string(tuning, difficulty)
         # print(note, string)
-        print(f'Where can u fret {NOTES[note]} on string {string}')
+        print(f"Where can u fret {NOTES[note]} on string {string}")
         answer = int(input())
         print((answer + courses[string]) % 12 == note)
 
 
-NOTES = ('C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B',)
+NOTES = (
+    "C",
+    "C#",
+    "D",
+    "Eb",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "Ab",
+    "A",
+    "Bb",
+    "B",
+)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-            description='Test your knowledge of the guitar fretboard')
+        description="Test your knowledge of the guitar fretboard"
+    )
     # Parse tuning
     parser.add_argument(
-            '-t', '--tuning',
-            # choices=['E standard'],
-            help=('choose tuning, e.g. "E standard" or "Drop D". '
-                  'Default is E standard'),
-            default='E standard',
-            )
+        "-t",
+        "--tuning",
+        # choices=['E standard'],
+        help=(
+            'choose tuning, e.g. "E standard" or "Drop D". '
+            "Default is E standard"
+        ),
+        default="E standard",
+    )
     # Parse difficulty
     parser.add_argument(
-            '-d', '--difficulty',
-            help='Determines how high on the fretboard this will go',
-            default='full'
-            )
+        "-d",
+        "--difficulty",
+        help="Determines how high on the fretboard this will go",
+        default="full",
+    )
 
     args = parser.parse_args()
     tuning = args.tuning
     tunings_dict = {
-            'E standard': (4, 11, 7, 2, 9, 4,),
-            'Drop D': (4, 11, 7, 2, 9, 2,),
-            }
+        "E standard": (4, 11, 7, 2, 9, 4,),
+        "Drop D": (4, 11, 7, 2, 9, 2,),
+    }
     difficulty = args.difficulty
     difficulties_dict = {
-            'easy': (0, 6),
-            'medium': (0, 8),
-            'hard': (0, 10),
-            'full': (0, 12),
-            }
-    return(tunings_dict[tuning], difficulties_dict[difficulty])
+        "easy": (0, 6),
+        "medium": (0, 8),
+        "hard": (0, 10),
+        "full": (0, 12),
+    }
+    return (tunings_dict[tuning], difficulties_dict[difficulty])
 
 
 def random_note_and_string(tuning, difficulty):
     minimum, maximum = difficulty
     course_number = np.random.randint(1, 7)
     fret_to_play = np.random.randint(minimum, maximum)
-    return ((fret_to_play + tuning[course_number - 1]) % 12,
-            course_number)
+    return ((fret_to_play + tuning[course_number - 1]) % 12, course_number)
 
 
 def make_courses(tuning):
-    return {number: note
-            for number, note
-            in zip(range(1, 7), tuning)}
+    return {number: note for number, note in zip(range(1, 7), tuning)}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
