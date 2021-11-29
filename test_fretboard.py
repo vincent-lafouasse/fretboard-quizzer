db6210772a7040cfe460083aca2a46b7ab948658
vincent-lafouasse <vincentlafouasse@live.fr>
2021-11-29 13:47:07 +0100

    added test for Guitar class default tuning and for expected strings attribute

--- test_fretboard.py
+++ test_fretboard.py
@@ -2,7 +2,7 @@ import pytest
 from rich import inspect
 import fretboard as fret
 
-# Tuning
+# Guitar class
 def test_E_standard():
     guitar = fret.Guitar('E standard')
     assert tuple(guitar.strings.values()) == (4, 11, 7, 2, 9, 4,)
@@ -23,6 +23,19 @@ def test_retune():
     guitar.set_tuning('Drop D')
     assert tuple(guitar.strings.values()) == (4, 11, 7, 2, 9, 2,)
 
+def test_default_tuning():
+    guitar = fret.Guitar()
+    assert tuple(guitar.strings.values()) == (4, 11, 7, 2, 9, 4,)
+
+def test_string_dict():
+    guitar = fret.Guitar()
+    expected = {1: 4,
+            2: 11,
+            3: 7,
+            4: 2,
+            5: 9,
+            6: 4,}
+    assert guitar.strings == expected
 
 if __name__ == '__main__':
     test_invalid_tuning()
