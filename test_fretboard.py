bde383c51ad3cd8761c3da65fde0114c13bac79b
vincent-lafouasse <vincentlafouasse@live.fr>
2021-11-29 13:58:27 +0100

    change test function names to reflect which class is tested

--- test_fretboard.py
+++ test_fretboard.py
@@ -4,44 +4,44 @@ import fretboard as fret
 
 # Quiz class
 
-def test_easy_setting():
+def test_quiz_easy_setting():
     quiz = fret.Quiz('easy')
     assert quiz.frets == (0, 6)
 
-def test_hard_setting():
+def test_quiz_hard_setting():
     quiz = fret.Quiz('hard')
     assert quiz.frets == (0, 10)
 
-def test_invalid_setting():
+def test_quiz_invalid_setting():
     with pytest.raises(AssertionError) as exc_info:
         quiz = fret.Quiz('why am i here i play the trombone')
 
 # Guitar class
-def test_E_standard():
+def test_guitar_E_standard():
     guitar = fret.Guitar('E standard')
     assert tuple(guitar.strings.values()) == (4, 11, 7, 2, 9, 4,)
 
 
-def test_drop_D():
+def test_guitar_drop_D():
     guitar = fret.Guitar('Drop D')
     assert tuple(guitar.strings.values()) == (4, 11, 7, 2, 9, 2,)
 
 
-def test_invalid_tuning():
+def test_guitar_invalid_tuning():
     with pytest.raises(AssertionError) as exc_info:
         guitar = fret.Guitar('420')
 
-def test_retune():
+def test_guitar_retune():
     guitar = fret.Guitar('E standard')
     assert tuple(guitar.strings.values()) == (4, 11, 7, 2, 9, 4,)
     guitar.set_tuning('Drop D')
     assert tuple(guitar.strings.values()) == (4, 11, 7, 2, 9, 2,)
 
-def test_default_tuning():
+def test_guitar_default_tuning():
     guitar = fret.Guitar()
     assert tuple(guitar.strings.values()) == (4, 11, 7, 2, 9, 4,)
 
-def test_string_dict():
+def test_guitar_string_dict():
     guitar = fret.Guitar()
     expected = {1: 4,
             2: 11,
